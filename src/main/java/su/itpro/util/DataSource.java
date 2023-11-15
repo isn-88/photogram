6dabcb718add3a3a6896a07034fb9b5279d5247f
Stanislav Ivaschenko <s.ivaschenko@netris.ru>
2023-11-15 20:59:47 +0700

    feat: add hikari connection pool

---
+++ src/main/java/su/itpro/util/DataSource.java
@@ -0,0 +1,20 @@
+package su.itpro.util;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public final class DataSource {
+
+  private static final HikariConfig CONFIG = new HikariConfig(PropertiesUtil.getHikariProperties());
+  private static final HikariDataSource CONNECTION_POOL = new HikariDataSource(CONFIG);
+
+  private DataSource() {
+  }
+
+  public static Connection getConnection() throws SQLException {
+    return CONNECTION_POOL.getConnection();
+  }
+
+}
