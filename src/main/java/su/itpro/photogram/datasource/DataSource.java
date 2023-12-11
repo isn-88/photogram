a7aba7903da54a93000c0c52cc9339b7e530fc63
Stanislav Ivaschenko <s.ivaschenko@netris.ru>
2023-12-11 22:31:00 +0700

    feat: use custom db connection pool

--- src/main/java/su/itpro/photogram/datasource/DataSource.java
+++ src/main/java/su/itpro/photogram/datasource/DataSource.java
@@ -1,31 +1,69 @@
 package su.itpro.photogram.datasource;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import su.itpro.photogram.util.PropertiesUtil;
 
 public class DataSource {
 
+  private static final int POOL_SIZE_DEFAULT = 10;
+  private static final String DB_DRIVER_NAME_KEY = "db.driver.name";
+  private static final String DB_USER_KEY = "db.user";
+  private static final String DB_PASSWORD_KEY = "db.password";
+  private static final String DB_URL_KEY = "db.url";
+  private static final String POOL_SIZE_KEY = "db.pool.size";
+  private static BlockingQueue<Connection> pool;
+
   static {
-    try {
-      Class.forName("org.postgresql.Driver");
-    } catch (ClassNotFoundException e) {
-      throw new RuntimeException(e);
-    }
+    loadDriver();
+    initConnectionPool();
   }
-
-  private static final HikariConfig CONFIG = new HikariConfig(PropertiesUtil.getHikariProperties());
-
-  private static final HikariDataSource CONNECTION_POOL = new HikariDataSource(CONFIG);
 
 
   private DataSource() {
   }
 
-  public static synchronized Connection getConnection() throws SQLException {
-    return CONNECTION_POOL.getConnection();
+  public static Connection getConnection() {
+    try {
+      return pool.take();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static void closeConnectionPool() {
+    pool.forEach(connection -> ((ConnectionWrapper) connection).closeConnection());
+  }
+
+  private static void loadDriver() {
+    try {
+      Class.forName(PropertiesUtil.getProperty(DB_DRIVER_NAME_KEY));
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static void initConnectionPool() {
+    int size = PropertiesUtil.getInt(POOL_SIZE_KEY, POOL_SIZE_DEFAULT);
+    pool = new ArrayBlockingQueue<>(size);
+    for (int i = 0; i < size; i++) {
+      pool.add(new ConnectionWrapper(openConnection(), pool));
+    }
+  }
+
+  private static Connection openConnection() {
+    try {
+      return DriverManager.getConnection(
+          PropertiesUtil.getProperty(DB_URL_KEY),
+          PropertiesUtil.getProperty(DB_USER_KEY),
+          PropertiesUtil.getProperty(DB_PASSWORD_KEY)
+      );
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 
 }
